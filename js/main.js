4a51267c100054e0267eb78229f6969cbbd99fe9
Doyeon-97 <ehdus0918@gmail.com>
2021-07-20 17:04:38 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
 
-//변수선언
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-//이벤트가 온 뒤에는 항상 function
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-//classList.contains: 값이 존재하는지 체크한다(true/false)
-//classList.toggle(): 클래스 값이 있는지 체크하고 없으면 더하고 있으면 제거한다.
-
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -163,6 +142,3 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller()); 
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
